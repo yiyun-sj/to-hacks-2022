a016d570f764403500608c01b37d69bc00608ec8
yiyun-sj <67709970+yiyun-sj@users.noreply.github.com>
2022-05-28 13:44:38 -0400

    restructured routes and ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,50 @@ import { firebaseConfig } from './constants/firebaseConstants'
 import { getAuth } from 'firebase/auth'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import axios from "axios"
+import axios from 'axios'
+import Home from './pages/Client/Home'
+import SignIn from './pages/Host/SignIn'
+import HostHome from './pages/Host/HostHome'
 
 initializeApp(firebaseConfig)
 
-export default function App() {
+function Client() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<h1>Nothing Here</h1>} />
+    </Routes>
+  )
+}
+
+function Host() {
   const auth = getAuth()
   const [user] = useAuthState(auth)
 
   return (
     <>
       {user ? (
-        <Router>
-          <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='*' element={<h1>Nothing Here</h1>} />
-            </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<HostHome />} />
+          <Route path="*" element={<HostHome />} />
+        </Routes>
       ) : (
-        <Router>
-          <Routes>
-              <Route path='/' element={<SignIn />} />
-              <Route path='*' element={<h1>Nothing Here</h1>} />
-            </Routes>
-        </Router>
+        <Routes>
+          <Route path="/*" element={<SignIn />} />
+        </Routes>
       )}
     </>
+  )
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Client />} />
+        <Route path="/host" element={<Host />} />
+        <Route path="*" element={<h1>Nothing Here</h1>} />
+      </Routes>
+    </Router>
   )
 }
